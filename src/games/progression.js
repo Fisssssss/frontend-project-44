5437d9a6c18b0c003632aa6ec0aa3d0da8adefbb
Antoshhja <dan1206fis@gmail.com>
2023-09-22 14:13:46 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,22 +1,29 @@
 import startGame from '../engine.js';
 import { getRandomNumber } from '../tools.js';
 
-const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const gameDescription = 'What number is missing in the progression?';
 
-const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return false;
+const getProgression = () => {
+  const arr = [];
+  const progressionStep = getRandomNumber(1, 15);
+  const progressionLength = getRandomNumber(5, 10);
+  for (let i = progressionStep; i <= progressionStep * progressionLength; i += progressionStep) {
+    arr.push(i);
   }
-  return true;
-};
-
-const primeGame = () => {
-  const question = getRandomNumber();
-  const correctAnswer = isPrime(question) ? 'yes' : 'no';
-
+  const secretSpot = getRandomNumber(0, arr.length - 1);
+  const correctAnswer = arr[secretSpot];
+  arr[secretSpot] = '..';
+  const question = arr.join(' ');
   return [question, correctAnswer];
 };
 
-const start = () => startGame(gameDescription, primeGame);
+const progressionGame = () => {
+  const answers = getProgression();
+  const [question, correctAnswer] = answers;
+
+  return [question, String(correctAnswer)];
+};
+
+const start = () => startGame(gameDescription, progressionGame);
 
 export default start;
